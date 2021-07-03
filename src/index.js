158307f9430531b49178061a0ad83d150e50875b
ailevchenko <levkadev@gmail.com>
2021-07-03 13:53:14 +0300

    HomeWork-1. Completed 1, 2, 3 tasks.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,123 @@
 import './assets/style/index.scss';
+
+import Arthas from './assets/img/Male-3-Walk.png';
+
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+const widthArea = canvas.clientWidth;
+const heightArea = canvas.clientHeight;
+
+const spriteW = 48;
+const spriteH = 48;
+
+const step = 10;
+
+const shots = 3;
+let cycle = 0;
+
+let bottomPressed = false;
+let upPressed = false;
+let rightPressed = false;
+let leftPressed = false;
+
+let pX = widthArea / 2 - spriteW / 2;
+let pY = heightArea / 2 - spriteH / 2;
+
+let direction = 0;
+
+const buttonState = (key, pressed) => {
+  switch (key) {
+    case 'Down':
+    case 'ArrowDown':
+      bottomPressed = pressed;
+      break;
+    case 'Up':
+    case 'ArrowUp':
+      upPressed = pressed;
+      break;
+    case 'Left':
+    case 'ArrowLeft':
+      leftPressed = pressed;
+      break;
+    case 'Right':
+    case 'ArrowRight':
+      rightPressed = pressed;
+      break;
+    default:
+      break;
+  }
+};
+
+const keyDownHandler = (e) => {
+  buttonState(e.key, true);
+};
+
+const keyUpHandler = (e) => {
+  buttonState(e.key, false);
+};
+
+const changeCycle = () => {
+  cycle = (cycle + 1) % shots;
+};
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+
+const img = document.createElement('img');
+img.src = Arthas;
+
+const getPosition = (newPosition) => {
+  if (newPosition < 0) {
+    return 0;
+  }
+
+  if (newPosition + 48 > 600) {
+    return newPosition - step;
+  }
+
+  return newPosition;
+};
+
+const changeDirection = (newDirection) => {
+  direction = direction !== newDirection ? newDirection : direction;
+};
+
+const changePosition = () => {
+  if (bottomPressed) {
+    pY = getPosition(pY + step);
+
+    changeDirection(0);
+    changeCycle();
+  }
+
+  if (upPressed) {
+    pY = getPosition(pY - step);
+
+    changeDirection(144);
+    changeCycle();
+  }
+
+  if (rightPressed) {
+    pX = getPosition(pX + step);
+
+    changeDirection(96);
+    changeCycle();
+  }
+
+  if (leftPressed) {
+    pX = getPosition(pX - step);
+
+    changeDirection(48);
+    changeCycle();
+  }
+};
+
+img.addEventListener('load', () => {
+  setInterval(() => {
+    changePosition();
+
+    ctx.clearRect(0, 0, 600, 600);
+    ctx.drawImage(img, cycle * spriteW, direction, spriteW, spriteH, pX, pY, 48, 48);
+  }, 20);
+});
